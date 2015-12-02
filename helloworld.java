30fa6722790eaa1ed227f34398715eccb5b07338
gpichai <gpichai2006@gmail.com>
2015-12-01 18:06:17 -0800

    test

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Goodbye, World!");
+        System.out.println("Goodbye, World from branch");
 
     }
 
